4004f49758f99949b34a3275725f818ec35e3a2f
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-12-14 19:44:25 +0100

    static files

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const bodyParser = require('body-parser')
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use('/static', express.static('static'))
 
+const home = require('./routes/homeRouter')
 
+app.use(home)
 
 const port = 3000
 app.listen(port)
