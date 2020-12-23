596823d904817271a7e0b2d409fa7ee8571be1e8
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-12-23 20:08:11 +0100

    bcrypt

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -1,8 +1,27 @@
 const router = require('express').Router()
 const adminMiddleware = require('../middleware/adminMiddleware')
+const { v4: uuidv4 } = require('uuid')
+const dbCon = require('../database')
+const bcrypt = require('bcrypt')
 
 router.get('/register', adminMiddleware, (req, res) => {
-    res.render('register')
+    res.render('register', { isAdded: '' })
+})
+
+router.post('/registerForm', (req, res) => {
+    const emri = req.body.emri
+    const mbiemri = req.body.mbiemri
+    const email = req.body.email
+    const password = req.body.password
+    const kontakti = req.body.kontakti
+    const verified = false
+    const uuid = uuidv4()
+
+    bcrypt.hash(password, 10, (err, hash) => {
+        const query = `INSERT INTO mbikqyresit(emri,mbiemri,email,password,kontakti,uuid,verified) VALUES("${emri}","${mbiemri}","${email}","${hash}","${kontakti}","${uuid}","${verified}")`
+        dbCon.execute(query)
+        res.render('register', { isAdded: 'u shtua ne databaze' })
+    })
 })
 
 module.exports = router
