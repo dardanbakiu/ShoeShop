e4daed046041ff012ce9d5115a3f2c6973b3eb66
dardanbakiu <dardan.bakiu@gmail.com>
2021-01-20 18:57:29 +0100

    kategoria

--- routes/kategoriaRoute.js
+++ routes/kategoriaRoute.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const dbCon = require('../database')
 
+router.get('/produktet', (req, res) => {
+    const query = `SELECT * FROM kepucet`
+    dbCon.query(query, (err, result, field) => {
+        res.render('kategoria', { kepucet: result })
+    })
+})
+
 router.get('/produktet/:kategoria', (req, res) => {
     const kategoria = req.params.kategoria
     const query = `SELECT * FROM kepucet WHERE firma="${kategoria}"`
